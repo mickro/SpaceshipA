4f401a8e63e2f918218e411e1e89fe7b715a64c9
Mickael Foucaux <mickael.foucaux@gmail.com>
2014-08-27 11:03:06 +0200

    More galactic design

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
+const PIPE_VELOCITY = -200;
 
 var mainState = {
 
   preload: function () {
-    game.stage.backgroundColor = '#71c5cf';
+    game.stage.backgroundColor = '#113';
 
-    game.load.image('bird', 'assets/bird.png');
+    game.load.image('ship', 'assets/ship.png');
     game.load.image('pipe', 'assets/pipe.png');
+    game.load.image('star', 'assets/star.png');
     
+    game.load.spritesheet('flame', 'assets/flame_sheet.png', 15, 15);
+
     game.load.audio('jump', 'assets/jump.wav'); 
   },
 
@@ -16,76 +20,139 @@ var mainState = {
     
     this.jumpSound = game.add.audio('jump');
 
+    this.stars = game.add.group();
+    this.stars.enableBody = true;
+    this.stars.createMultiple(20, 'star');
+    this.timerStars = this.game.time.events.loop(500, this.addStar, this);
+    
     this.pipes = game.add.group();
     this.pipes.enableBody = true;
-    this.pipes.createMultiple(20, 'pipe');  
-    this.timer = this.game.time.events.loop(1500, this.addRowOfPipes, this);           
+    this.pipes.createMultiple(20, 'pipe');
+    this.timer = this.game.time.events.loop(2500, this.addRowOfPipes, this);
 
-    this.bird = this.game.add.sprite(100, 245, 'bird');
-    game.physics.arcade.enable(this.bird);
-    this.bird.body.gravity.y = 1000;
-    this.bird.anchor.setTo(-0.2, 0.5);  
-    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    spaceKey.onDown.add(this.jump, this); 
+
+    
+    var ship_x = 100;
+    var ship_y = 245;
+    this.flame = this.game.add.sprite(ship_x, ship_y -8, 'flame');
+    this.flame.animations.add('boom', [1, 2, 3, 2]);
+    this.flame.animations.play('boom', 10, true);
+    
+    this.ship = this.game.add.sprite(ship_x, ship_y, 'ship');
+    game.physics.arcade.enable(this.ship);
+    this.ship.body.gravity.y = 0;
+    this.ship.anchor.setTo(-0.2, 0.5);
+
+    // init keyboard
+    var upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    upKey.onDown.add(this.goUp, this);
+
+    var downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    downKey.onDown.add(this.goDown, this);
 
     this.score = 0;
-    this.labelScore = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });  
+    this.labelScore = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
   },
 
   update: function() {
-    if (this.bird.inWorld == false)
-      this.restartGame(); 
+    if (this.ship.inWorld === false)
+      this.restartGame();
 
-    game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+    if (this.ship.alive) { 
+      game.physics.arcade.overlap(this.ship, this.pipes, this.hitPipe, null, this);
 
-    if (this.bird.angle < 20)
-      this.bird.angle += 1;
+      if (this.ship.angle < 0)
+        this.ship.angle += 1;
+
+      if (this.ship.angle > 0)
+        this.ship.angle -= 1;
+    }
+
+    this.flame.x =this.ship.x;
+    this.flame.y =this.ship.y -8;
   },
 
-  jump: function() {
-    if (this.bird.alive == false) return;
+  goUp: function() {
+    if (this.ship.alive == false) return;
     
-    this.bird.body.velocity.y = -350;
+    this.ship.body.velocity.y -= 50;
 
-    // Create an animation on the bird
-    var animation = game.add.tween(this.bird);
+    if (this.ship.body.velocity.y < -250) this.ship.body.velocity.y = -150;
+      
+    // Create an animation on the ship
+    var animation = game.add.tween(this.ship);
 
     // Set the animation to change the angle of the sprite to -20° in 100 milliseconds
     animation.to({angle: -20}, 100);
 
     // And start the animation
     animation.start();
+  },
+  
+  goDown: function() {
+    if (this.ship.alive == false) return;
     
-    this.jumpSound.play();
+    this.ship.body.velocity.y += 50;
+    
+    if (this.ship.body.velocity.y > 250) this.ship.body.velocity.y = 250;
+
+    // Create an animation on the ship
+    var animation = game.add.tween(this.ship);
+
+    // Set the animation to change the angle of the sprite to -20° in 100 milliseconds
+    animation.to({angle: 20}, 100);
+
+    // And start the animation
+    animation.start();
   },
 
   restartGame: function() {
     game.state.start('main');
   },
+  
+  addStar: function() {
+    var star = this.stars.getFirstDead();
+    var r = Math.random();
+    star.reset(400, Math.floor(r * 360 ) + 20);
+    
+    vx = -100;
+    if ((r < 0.3) || (r > 0.7)) 
+      vx = -120;
+    else if ((r > 0.45) && (r < 0.55))
+      vx = -80;
+    
+    star.body.velocity.x = vx;
+    star.checkWorldBounds = true;
+    star.outOfBoundsKill = true;
+  },
 
   addOnePipe: function(x, y) {
     var pipe = this.pipes.getFirstDead();
+  
+    var hole = Math.floor(Math.random()*5)+1;
 
     pipe.reset(x, y);
-    pipe.body.velocity.x = -200;     
+    pipe.body.velocity.x = PIPE_VELOCITY;     
     pipe.checkWorldBounds = true;
     pipe.outOfBoundsKill = true;
   },
 
   hitPipe: function() {  
-    // If the bird has already hit a pipe, we have nothing to do
-    if (this.bird.alive == false) return;
+    // If the ship has already hit a pipe, we have nothing to do
+    if (this.ship.alive == false) return;
 
-    // Set the alive property of the bird to false
-    this.bird.alive = false;
+    // Set the alive property of the ship to false
+    this.ship.alive = false;
+    
+    this.ship.body.gravity.y = 0;
+    this.ship.body.velocity.y = 0;
 
     // Prevent new pipes from appearing
     game.time.events.remove(this.timer);
+    game.time.events.remove(this.timerStars);
 
-    // Go through all the pipes, and stop their movement
-    this.pipes.forEachAlive(function(p){
-      p.body.velocity.x = 0;
-    }, this);
+    // kick out the ship
+    this.ship.body.velocity.x = PIPE_VELOCITY;
   },
 
   addRowOfPipes: function() {
@@ -96,25 +163,12 @@ var mainState = {
         this.addOnePipe(400, i*60+10);   
 
     this.score += 1;
-    this.labelScore.text = this.score;  
+    this.labelScore.text = this.score;
+
+     game.stage.backgroundColor = '#111';
+     setTimeout(function(){game.stage.backgroundColor = '#113';}, 150);
   },
 
-  hitPipe: function() {  
-    // If the bird has already hit a pipe, we have nothing to do
-    if (this.bird.alive == false)
-      return;
-
-    // Set the alive property of the bird to false
-    this.bird.alive = false;
-
-    // Prevent new pipes from appearing
-    game.time.events.remove(this.timer);
-
-    // Go through all the pipes, and stop their movement
-    this.pipes.forEachAlive(function(p){
-      p.body.velocity.x = 0;
-    }, this);
-  },
 };
 
 game.state.add('main', mainState);  
---
+++ readme.md
@@ -0,0 +1,7 @@
+= Spaceship A =
+
+is horizontal scrolling
+and collision game
+
+made with Phaser
+> HTML5 game engine <
